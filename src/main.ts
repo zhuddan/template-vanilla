8c9a2134f20f9c21be45773122dc9c8453c27e6e
zhuddan <yobaci@163.com>
2024-10-25 11:18:30 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import './style.css'
+import { App, Text } from '@zd~/shapex'
 
-const app = document.getElementById('app')!
+const app = new App()
+const t = new Text({
+  text: '@zd~/shapex',
+  style: {
+    fontSize: 18,
+  },
+})
+t.position.set(200)
+app.add(t)
+document.body.appendChild(app.canvas)
 
-function init() {
-  updateAppText(new Date())
-  requestAnimationFrame(init)
-}
-
-function updateAppText(date: Date) {
-  const currText = app.textContent
-  const nextText = `[vanilla js] now is ${date.toLocaleTimeString()}`
-  if (currText !== nextText) {
-    app.textContent = nextText
+let d = 1
+function update() {
+  t.rotation += 0.01
+  const scale = 0.01
+  t.scale.set(t.scale.x += d * scale)
+  if (t.scale.x > 2 || t.scale.x < -2) {
+    d *= -1
   }
+  requestAnimationFrame(update)
 }
-
-init()
+requestAnimationFrame(update)
